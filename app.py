69bf66ffaa03809b2f4c10e6e1a25e02988bfac6
Ola Malmgren <malmgrenola@gmail.com>
2021-08-10 14:20:47 +0200

    wire up mongoDB and heroku

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def index():
     return render_template("index.html",page_title="Home")
 
+
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "3000")),
-        debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
