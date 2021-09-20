dd324c1af80040d17c5915796f8e977e7e12a255
Ola Malmgren <malmgrenola@gmail.com>
2021-09-20 11:06:34 +0200

    style: fix active in navbar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  console.log("Hello JQuery");
+  // Handle active state navbar
+  const firstPath = window.location.pathname.split("/")[1];
+
+  if (firstPath === "admin") {
+    $(`.nav-link[href="/admin"]`).addClass("active");
+    return;
+  }
+
+  if (firstPath === "me") {
+    $(`.nav-link[href="/me/overview"]`).addClass("active");
+    return;
+  }
+
+  $(`.nav-link[href="${window.location.pathname}"]`).addClass("active");
 });
 
 // Search Helpers
